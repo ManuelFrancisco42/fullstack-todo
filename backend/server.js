d6fb0ca2c4476b79bb58f2be86b3677755814bf2
Manuel Francisco <ciesafrancisco@hotmail.com>
2026-02-02 21:50:55 +0100

    Prepare for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const startApp = async () => {
   try {
     await connectDB()
     
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(
     `Server is Up and Running on port http://localhost:${PORT}☑️`.green.bold
       .underline,
